da0f1844418ec3abe05b3895e1ae1eb474efb610
bencrowder <ben.crowder@gmail.com>
2014-02-28 10:09:46 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='fledge',
+      version='0.1',
+      description='File processing language/shell',
+      author='Ben Crowder',
+      author_email='ben@bencrowder.net',
+      url='http://bencrowder.net/coding/fledge',
+      packages=['fledge', 'fledge.actions'],
+      )
